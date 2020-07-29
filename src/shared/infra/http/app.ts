238762e843d291441ed363c0408d79b6d1f6cae5
Ana Paula Menezes <anapaularossi.577@gmail.com>
2020-07-29 07:29:53 -0300

    saving orders

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-
+import { errors } from 'celebrate';
 import AppError from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 import routes from './routes';
@@ -15,8 +15,10 @@ createConnection();
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
